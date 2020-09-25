bb292ec4d106a2e52a7b3f49107d10624e724769
dlonra22 <arnold.bwaila@gmail.com>
2020-09-25 14:32:56 +0100

    stubbed out users controller with empty routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :items
-  resources :transcations
+
   resources :users, only:[:show] do
-      resources :transcations, only: [:index, :new, :show]
+      resources :transcations, only: [:index, :new, :show, :create, :destroy]
   end
 
+  resources :users
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
