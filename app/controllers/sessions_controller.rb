84b06f81899db3c7073d1e57b9081bd2629e4ea1
dlonra22 <arnold.bwaila@gmail.com>
2020-09-26 16:27:10 +0100

    added css to application stysheet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
             redirect_to user_path(current_user)
         end
     end
+
     def create
         user = User.find_by(username: params[:user][:username])
         if user.try(:authenticate, params[:user][:password])
