c185561f91a77d54b25b5009e8232e268203b2bd
dlonra22 <arnold.bwaila@gmail.com>
2020-09-26 11:22:01 +0100

    implemented application helper current_user to track when a user is logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-    def current_user
+    def current_user #returns user object or nil
         if session[:user_id]
             user=User.find_by(id: session[:user_id])
         else
             user = nil
         end
-        user
+        user 
     end
+
 end
