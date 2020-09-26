1b7ccb47aef2252dec9f6fbfcad900b1d06e7d85
dlonra22 <arnold.bwaila@gmail.com>
2020-09-26 12:29:46 +0100

    added sign logic for user in sessions create action including flash error and redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
     def login
     end
     def create
+        user = User.find_by(username: params[:username])
+        if user.try(:authenticate, params[:password])
+            session[:user_id] = user.id 
+            redirect_to user_path(user)
+        else
+            flash[:error] ="Invalid login details, please try again"
+            redirect_to login_path
+        end
     end
     def destroy
         session.delete :user_id
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
     def current_user #returns user object or nil
         if session[:user_id]
-            user=User.find_by(id: session[:user_id])
+            @c_user ||= User.find_by(id: session[:user_id])
         else
-            user = nil
+            @c_user = nil
         end
-        user 
+        @c_user 
     end
 
 end
