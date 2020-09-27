0f80b14d2ccef5785e01f3b6d3c382bf3e7b8d87
dlonra22 <arnold.bwaila@gmail.com>
2020-09-27 19:36:05 +0100

    updated usrs controller new action to cater for when a user is logged in or not or is an admin, only admins allowed to create when logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
    has_many :transactions
    has_many :items, through: :transactions
    has_secure_password
+
+   def self.admin_users
+     where(is_admin: true)
+   end
 end
